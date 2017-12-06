aba811b32bf8c5afed5119d90d6612f54ddf2175
Robert Franke <franke.rob@gmail.com>
2017-12-06 20:59:30 +0000

    Track progress

--- 05/run.sh
+++ 05/run.sh
@@ -1,3 +1,5 @@
 cythonize -3 day5.pyx
+echo "Compiling"
 $CC -O3 -shared -fPIC $(pkg-config --cflags --libs python3) -o day5.so day5.c
+echo "Running"
 python3 -m timeit -n 1 -c 'import day5; day5.main()'
