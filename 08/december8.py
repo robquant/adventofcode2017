9e4fa09b4ea618b724143b0611039c975e1293ae
Robert Franke <franke.rob@gmail.com>
2017-12-08 09:26:15 +0000

    Dec 8

---
+++ 08/december8.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+
+registers = defaultdict(int)
+
+def compare(regname, op, value):
+    if op == '<':
+        return registers[regname] < value
+    elif op =='<=':
+        return registers[regname] <= value
+    if op == '>':
+        return registers[regname] > value
+    elif op =='>=':
+        return registers[regname] >= value
+    if op == '==':
+        return registers[regname] == value
+    elif op =='!=':
+        return registers[regname] != value
+    else:
+        raise ValueError("Unknown operation {}".format(op))
+
+def change_register(regname, op, value):
+    if op == 'inc':
+        registers[regname] += value
+        return
+    elif op == 'dec':
+        registers[regname] -= value
+        return
+    raise ValueError("Unknown operation {}".format(op))
+
+def main(input):
+    highest_ever = 0
+    for line in input:
+        items = line.split(' ')
+        input_register = items[0]
+        op = items[1]
+        assert op == 'inc' or op == 'dec'
+        op_value = int(items[2])
+        if_ = items[3]
+        assert if_ == 'if'
+        comp_register = items[4]
+        comp_op = items[5]
+        assert comp_op in ('<', '<=', '>', '>=', '==', '!=')
+        comp_val = int(items[6])
+        if compare(comp_register, comp_op, comp_val):
+            change_register(input_register, op, op_value)
+        highest_ever = max(highest_ever, max(registers.values()))
+    print(max(registers.values()), highest_ever)
+
+
+if __name__ == '__main__':
+    input = open("december8_input.txt").readlines()
+    main(input)
