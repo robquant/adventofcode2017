60e50960d14e6ce06c7c86f2095885e8506c2315
Robert Franke <robert.franke@nirx.de>
2017-12-12 16:29:21 +0100

    December 12 Part 1

--- 12/december12.py
+++ 12/december12.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def main():
     input = open("december12_input.txt").readlines()
     mappings = {}
@@ -6,7 +8,22 @@ def main():
         start = int(items[0])
         targets = [int(item.rstrip(',')) for item in items[2:]]
         mappings[start] = targets
-    print(mappings)
+    reachable = set()
+    checked = set()
+    reachable.add(0)
+    checked.add(0)
+    work = deque()
+    for item in mappings[0]:
+        work.append(item)
+        reachable.add(item)
+    while len(work) > 0:
+        item = work.popleft()
+        reachable.add(item)
+        if not item in checked:
+            for target in mappings[item]:
+                work.append(target)
+        checked.add(item)
+    print(len(reachable))
 
 if __name__ == '__main__':
     main()
