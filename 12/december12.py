598aa61e8da49effd66621516604e3d01860e507
Robert Franke <robert.franke@nirx.de>
2017-12-12 17:34:49 +0100

    Part 2

--- 12/december12.py
+++ 12/december12.py
@@ -1,19 +1,12 @@
 from collections import deque
 
-def main():
-    input = open("december12_input.txt").readlines()
-    mappings = {}
-    for line in input:
-        items = line.split(' ')
-        start = int(items[0])
-        targets = [int(item.rstrip(',')) for item in items[2:]]
-        mappings[start] = targets
+def find_group(mappings, start):
     reachable = set()
     checked = set()
-    reachable.add(0)
-    checked.add(0)
+    reachable.add(start)
+    checked.add(start)
     work = deque()
-    for item in mappings[0]:
+    for item in mappings[start]:
         work.append(item)
         reachable.add(item)
     while len(work) > 0:
@@ -23,7 +16,32 @@ def main():
             for target in mappings[item]:
                 work.append(target)
         checked.add(item)
+    return reachable
+
+
+def main():
+    input = open("december12_input.txt").readlines()
+    mappings = {}
+    for line in input:
+        items = line.split(' ')
+        start = int(items[0])
+        targets = [int(item.rstrip(',')) for item in items[2:]]
+        mappings[start] = targets
+    
+    not_checked = set(mappings.keys())
+    reachable = find_group(mappings, 0)
     print(len(reachable))
+
+    groups = 1
+    not_checked = not_checked.difference(reachable)
+    while len(not_checked) > 0:
+        item = not_checked.pop()
+        reachable = find_group(mappings, item)
+        groups += 1
+        not_checked = not_checked.difference(reachable)
+    print(groups)
+        
+    
 
 if __name__ == '__main__':
     main()
