d2d37a453a04e9a1676c0c53626ff983dd5fb0a7
Robert Franke <franke.rob@gmail.com>
2017-12-18 20:24:44 +0100

    Dec 17 part2

--- 17/december17.py
+++ 17/december17.py
@@ -1,6 +1,28 @@
 def main():
+    print(part1())
+    print(part2())
+
+def part2():
     input = 312
     buffer = []
+    pos = 0
+    final = 0
+    for n in range(int(50e6)):
+        if n > 0:
+            pos = (pos + input + 1) % n
+        if pos == 0:
+            final = n
+    return final
+
+def part1():
+    input = 312
+    buffer = []
+    pos = 0
+    for n in range(2018):
+        if n > 0:
+            pos = (pos + input + 1) % n
+        buffer.insert(pos, n)
+    return buffer[buffer.index(2017) + 1]
 
 if __name__ == '__main__':
     main()
