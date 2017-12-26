742ef585b5e08ffa215544eb276dd8997187dc86
Robert Franke <franke.rob@gmail.com>
2017-12-26 22:15:41 +0100

    Work on Dec 22

--- 22/december22.py
+++ 22/december22.py
@@ -10,32 +10,31 @@ def turn_right(direction):
     return Dir(direction.y, -direction.x)
 
 def infected(field, pos):
-    return field[pos.y][pos.x] == '#'
+    return field[len(field) - pos.y][pos.x] == '#'
 
 def infect(field, pos):
-    field[pos.y][pos.x] = '#'
+    field[len(field) - pos.y][pos.x] = '#'
 
 def clean(field, pos):
-    field[pos.y][pos.x] = '.'
+    field[len(field) - pos.y][pos.x] = '.'
 
 def step(field, pos, direction):
     if infected(field, pos):
-        print("Turning right: ", direction)
         direction = turn_right(direction)
-        print(direction)
         clean(field, pos)
+        did_infect = False
     else: # clean
-        print("Turning left: ", direction)
         direction = turn_left(direction)
-        print(direction)
         infect(field, pos)
+        did_infect = True
     pos = Pos(pos.x + direction.x, pos.y + direction.y)
-    print("Infected: ", infected(field, pos))
-    return pos, direction
+    return pos, direction, did_infect
 
 def part1(input):
     l = len(input) // 2
-    pos = (l, l)
+    pos = Pos(l, l)
+    direction = Dir(0, 1)
+    print(run(input, pos, direction, 10000))
 
 def load(fname):
     input = [list(line.rstrip('\n')) for line in open(fname)]
@@ -45,21 +44,29 @@ def test_part1():
     input = load("test.txt")
     pos = Pos(4, 4)
     direction = Dir(0, 1)
-    for _ in range(4):
-        pos, direction = step(input, pos, direction)
-        print_field(input, pos)
-        print('--')
+    assert run(input, pos, direction, 70) == 41
+
+def run(field, pos, direction, steps, debug=False):
+    infection_counter = 0
+    for _ in range(steps):
+        pos, direction, did_infect = step(field, pos, direction)
+        if did_infect:
+            infection_counter += 1
+        if debug:
+            print_field(field, pos)
+            print('--')
+    return infection_counter
 
 def print_field(field, pos):
     for i, line in enumerate(field):
-        if i == pos.y:
+        if i == len(field) - pos.y:
             print(' '.join(line[:pos.x]) + '[' + line[pos.x] + ']' + ' '.join(line[pos.x+1:]))
         else:
             print(' '.join(line))
 
 def main():
-    # input = load('december22_input.txt')
-    test_part1()
+    input = load('december22_input.txt')
+    part1(input)
 
 if __name__ == '__main__':
     main()
